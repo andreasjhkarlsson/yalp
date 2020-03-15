9f0094c74005bf3964d5e9e43ce2923ec11a2e9f
Andreas Karlsson <andreas@jhkarlsson.com>
2020-03-15 21:52:47 +0100

    Simple GC

--- yalp.c
+++ yalp.c
@@ -11,8 +11,9 @@ struct sexpr* eval_sexpr(struct env* env, struct sexpr* sexpr);
 struct sexpr* eval_argument(struct env* env, struct sexpr* args, int n);
 struct sexpr* eval_type_argument(struct env* env, struct sexpr* args, int n, enum sexpr_t type);
 struct sexpr* call_lambda(struct env* env, struct sexpr* lambda, struct sexpr* args);
-struct sexpr* read_sexpr(const char** str);
-struct sexpr* create_list(int element_count, ...);
+struct sexpr* read_sexpr(struct env* env, const char** str);
+struct sexpr* create_list(struct env* env, int element_count,  ...);
+struct sexpr* alloc_sexpr(struct env* env);
 
 enum sexpr_t
 {
@@ -31,8 +32,15 @@ enum function_t
     lambda
 };
 
+enum memory_mode_t
+{
+    untracked,
+    tracked
+};
+
 struct sexpr
 {
+    enum memory_mode_t memory_mode;
     enum sexpr_t tag;
     union
     {
@@ -64,50 +72,50 @@ struct sexpr
         } function;
     };
 }
-    _NIL = { .tag = nil },
-    _S_TRUE = { .tag = boolean, .boolean = true},
-    _S_FALSE = { .tag = boolean, .boolean = false};
+    _NIL = { .memory_mode = untracked, .tag = nil },
+    _S_TRUE = { .memory_mode = untracked, .tag = boolean, .boolean = true},
+    _S_FALSE = { .memory_mode = untracked, .tag = boolean, .boolean = false};
 
 struct sexpr* NIL = &_NIL;
 struct sexpr* S_TRUE = &_S_TRUE;
 struct sexpr* S_FALSE = &_S_FALSE;
 
-struct sexpr* new_sexpr(enum sexpr_t tag)
+struct sexpr* new_sexpr(struct env* env, enum sexpr_t tag)
 {
-    struct sexpr* e = malloc(sizeof(struct sexpr));
-    memset(e, 0, sizeof(struct sexpr));
+    struct sexpr* e = alloc_sexpr(env);
 
     e->tag = tag;
+    e->memory_mode = tracked;
 
     return e;
 }
 
-struct sexpr* new_integer(int n)
+struct sexpr* new_integer(struct env* env, int n)
 {
-    struct sexpr* e = new_sexpr(integer);
+    struct sexpr* e = new_sexpr(env, integer);
     e->integer = n;
     return e;
 }
 
-struct sexpr* new_function(enum function_t tag)
+struct sexpr* new_function(struct env* env, enum function_t tag)
 {
-    struct sexpr* e = new_sexpr(function);
+    struct sexpr* e = new_sexpr(env, function);
     e->function.tag = tag;
     return e;
 }
 
-struct sexpr* new_symbol(const char* str, size_t length)
+struct sexpr* new_symbol(struct env* env, const char* str, size_t length)
 {
-    struct sexpr* e = new_sexpr(symbol);
+    struct sexpr* e = new_sexpr(env, symbol);
     e->name = malloc(length + 1);
     strncpy((char*)e->name, str, length);
     ((char*)e->name)[length] = '\0';
     return e;
 }
 
-struct sexpr* new_error(const char* message)
+struct sexpr* new_error(struct env* env, const char* message)
 {
-    struct sexpr* e = new_sexpr(error);
+    struct sexpr* e = new_sexpr(env, error);
     e->message = message;
     return e;
 }
@@ -141,6 +149,7 @@ struct frame* create_frame()
     frame->bindings = NULL;
     frame->binding_count = 0;
     frame->previous = NULL;
+    frame->context = NULL;
     return frame;
 }
 
@@ -213,10 +222,124 @@ void free_frame(struct frame* frame)
     free(frame);
 }
 
+struct block
+{
+    struct sexpr sexpr;
+    bool marked: 1;
+    bool used: 1;
+};
+
+#define HEAP_SIZE 65536
+
 struct env
 {
+    struct block heap[HEAP_SIZE];
     struct frame* stack;
 };
+
+size_t available_heap_space(struct env* env)
+{
+    size_t free_slots = 0;
+    for (int i=0;i<HEAP_SIZE;i++)
+    {
+        if (!env->heap[i].used)
+            free_slots++;
+    }
+
+    return free_slots;
+}
+
+struct sexpr* alloc_sexpr(struct env* env)
+{
+    for (int i=0; i<HEAP_SIZE;i++)
+    {
+        struct block* block = &env->heap[i];
+        if (!block->used)
+        {
+            memset(block, 0, sizeof(struct block));
+            block->used = true;
+            return &block->sexpr;
+        }
+    }
+
+    static struct sexpr memory_error = {.memory_mode = untracked, .tag = error};
+    memory_error.message = "Out of memory";
+
+    printf("Out of memory!\n");
+    return &memory_error;
+}
+
+void mark_sexpr(struct sexpr* sexpr)
+{
+    if (sexpr->memory_mode == tracked)
+    {
+        struct block* block = (struct block*) sexpr;
+        if (block->marked)
+            return; 
+        block->marked = true;
+    }
+
+    switch (sexpr->tag)
+    {
+        case list:
+            mark_sexpr(sexpr->list.head);
+            mark_sexpr(sexpr->list.tail);
+            break;
+        case function:
+            if (sexpr->function.tag == lambda)
+            {
+                mark_sexpr(sexpr->function.lambda.params);
+                mark_sexpr(sexpr->function.lambda.exprs);
+            }
+            break;
+    }
+}
+
+void mark_frame(struct frame* frame)
+{
+    for (int i=0;i<frame->binding_count;i++)
+    {
+        struct binding* binding = &frame->bindings[i];
+        if (binding->name && binding->value)
+        {
+            mark_sexpr(binding->value);
+        }
+    }
+    // TODO: Mark context
+
+    if (frame->previous)
+        mark_frame(frame->previous);
+}
+
+void mark_roots(struct env* env)
+{
+    mark_frame(env->stack);
+}
+
+void sweep_heap(struct env* env)
+{
+    for (int i=0;i<HEAP_SIZE;i++)
+    {
+        struct block* block = &env->heap[i];
+
+        if (!block->marked)
+        {
+            block->used = false;
+        }
+
+        block->marked = false;
+    }
+}
+
+void collect_garbage(struct env* env)
+{
+    size_t available_before = available_heap_space(env);
+    mark_roots(env);
+    sweep_heap(env);
+    size_t available_after = available_heap_space(env);
+
+    printf("GC collected %d objects, heap now has %d slots available\n", available_after - available_before, available_after);
+}
 
 struct sexpr* get_env_binding(struct env* env, const char* name)
 {
@@ -235,7 +358,7 @@ void remove_env_binding(struct env* env, const char* name, bool recursive)
 
 void add_env_builtin_function(struct env* env, const char* name, struct sexpr* (*fn) (struct env* env, struct sexpr*))
 {
-    struct sexpr* v = new_function(builtin);
+    struct sexpr* v = new_function(env, builtin);
     v->function.builtin.name = copy_string(name);
     v->function.builtin.fn = fn;
 
@@ -318,7 +441,7 @@ struct sexpr* read_boolean(const char** str)
     return false;
 }
 
-struct sexpr* read_integer(const char** str)
+struct sexpr* read_integer(struct env* env, const char** str)
 {
     if (is_digit(**str))
     {
@@ -328,7 +451,7 @@ struct sexpr* read_integer(const char** str)
             number = number * 10 + (**str) - '0';
             (*str)++;
         }
-        struct sexpr* atom = new_sexpr(integer);
+        struct sexpr* atom = new_sexpr(env, integer);
         atom->integer = number;
         return atom;
     }
@@ -363,11 +486,11 @@ bool is_operator(char c)
     }
 }
 
-struct sexpr* read_operator(const char** str)
+struct sexpr* read_operator(struct env* env, const char** str)
 {
     if (is_operator(**str))
     {
-        struct sexpr* s = new_symbol(*str, 1);
+        struct sexpr* s = new_symbol(env, *str, 1);
         (*str)++;
         return s;
     }
@@ -375,10 +498,10 @@ struct sexpr* read_operator(const char** str)
     return NULL;
 }
 
-struct sexpr* read_symbol(const char** str)
+struct sexpr* read_symbol(struct env* env, const char** str)
 {
     struct sexpr* s;
-    if ((s = read_operator(str)))
+    if ((s = read_operator(env, str)))
         return s;
 
     if (!is_symbol_character(**str) || is_digit(**str))
@@ -389,11 +512,11 @@ struct sexpr* read_symbol(const char** str)
     int count;
     for (count = 0;is_symbol_character((*str)[count]);count++);
 
-    s = new_symbol(*str, count);
+    s = new_symbol(env, *str, count);
     (*str) += count;
     return s;
 }
-struct sexpr* create_list(int element_count, ...)
+struct sexpr* create_list(struct env* env, int element_count, ...)
 {
     struct sexpr* head = NIL;
     struct sexpr* previous = NULL;
@@ -403,7 +526,7 @@ struct sexpr* create_list(int element_count, ...)
     for (int i=0;i<element_count;i++)
     {
         struct sexpr* element = va_arg(valist, struct sexpr*);
-        struct sexpr* cell = new_sexpr(list);
+        struct sexpr* cell = new_sexpr(env, list);
         cell->list.head = element;
         cell->list.tail = NIL;
         if (previous)
@@ -419,20 +542,20 @@ struct sexpr* create_list(int element_count, ...)
     return head;
 }
 
-struct sexpr* read_quote(const char** str)
+struct sexpr* read_quote(struct env* env, const char** str)
 {
     if (**str == '\'')
     {
         (*str)++;
-        struct sexpr* s = new_sexpr(symbol);
+        struct sexpr* s = new_sexpr(env, symbol);
         s->name = "quote";
-        return create_list(2, s, read_sexpr(str));
+        return create_list(env, 2, s, read_sexpr(env, str));
     }
 
     return NULL;
 }
 
-struct sexpr* read_list(const char** str)
+struct sexpr* read_list(struct env* env, const char** str)
 {
     if ((**str) == '(')
     {
@@ -446,8 +569,8 @@ struct sexpr* read_list(const char** str)
             if ((*str)[0] == ')')
                 break;
 
-            struct sexpr* cell = new_sexpr(list);
-            cell->list.head = read_sexpr(str);
+            struct sexpr* cell = new_sexpr(env, list);
+            cell->list.head = read_sexpr(env, str);
             cell->list.tail = NIL;
 
             if (previous)
@@ -468,29 +591,29 @@ struct sexpr* read_list(const char** str)
 
 }
 
-struct sexpr* read_sexpr(const char** str)
+struct sexpr* read_sexpr(struct env* env, const char** str)
 {
     struct sexpr* e = NULL;
 
     // Skip spaces
     skip_whitespace(str);
 
-    if ((e = read_integer(str)))
+    if ((e = read_integer(env, str)))
         return e;
 
-    if ((e = read_list(str)))
+    if ((e = read_list(env, str)))
         return e;
 
-    if ((e = read_quote(str)))
+    if ((e = read_quote(env, str)))
         return e;
 
     if ((e = read_boolean(str)))
         return e;
 
-    if ((e = read_symbol(str)))
+    if ((e = read_symbol(env, str)))
         return e;
 
-    return new_error("Syntax error");
+    return new_error(env, "Syntax error");
 }
 
 int as_integer(struct sexpr* e)
@@ -522,7 +645,7 @@ struct sexpr* eval_lambda(struct env* env, struct sexpr* args)
     struct sexpr* params = args->list.head;
     struct sexpr* body = args->list.tail;
 
-    struct sexpr* sexpr = new_function(lambda);
+    struct sexpr* sexpr = new_function(env, lambda);
     sexpr->function.lambda.params = params;
     sexpr->function.lambda.exprs = body;
 
@@ -537,7 +660,7 @@ struct sexpr* eval_defun(struct env* env, struct sexpr* args)
 
     if (s->tag != symbol)
     {
-        return new_error("First argument to defun must be symbol");
+        return new_error(env, "First argument to defun must be symbol");
     }
 
     struct sexpr* lambda = eval_lambda(env, args->list.tail);
@@ -553,10 +676,14 @@ struct sexpr* eval_if(struct env* env, struct sexpr* args)
 {
     struct sexpr* cond = eval_argument(env, args, 0);
 
+    size_t arg_count = list_length(args);
+
     if (as_bool(cond))
         return eval_argument(env, args, 1);
-    else
+    else if (arg_count > 2)
         return eval_argument(env, args, 2);
+
+    return NIL;
 }
 
 struct sexpr* eval_reduce(struct env* env, struct sexpr* args)
@@ -572,7 +699,7 @@ struct sexpr* eval_reduce(struct env* env, struct sexpr* args)
 
     struct sexpr* el;
     while ((el = next(&lst)))
-        state = eval_sexpr(env, create_list(3, fn, el, state));
+        state = eval_sexpr(env, create_list(env, 3, fn, el, state));
 
     return state;
 }
@@ -581,7 +708,7 @@ struct sexpr* eval_bool_operator(struct env* env, struct sexpr* args, bool (*op)
 {
     int arg_length = list_length(args);
     if (arg_length < 2)
-        return new_error("At least 2 args are needed for binary operator");
+        return new_error(env, "At least 2 args are needed for binary operator");
     bool result = true;
 
     struct sexpr* left = next(&args);
@@ -626,7 +753,7 @@ struct sexpr* eval_less(struct env* env, struct sexpr* args)
 
 struct sexpr* eval_int_operator(struct env* env, struct sexpr* args, int (*op) (int,int), int state)
 {
-    struct sexpr* result = new_integer(state);
+    struct sexpr* result = new_integer(env, state);
     struct sexpr* arg;
     while ((arg = next(&args)))
     {
@@ -703,7 +830,7 @@ struct sexpr* eval_list(struct env* env, struct sexpr* args)
     struct sexpr* el;
     while ((el = next(&args)))
     {
-        struct sexpr* cell = new_sexpr(list);
+        struct sexpr* cell = new_sexpr(env, list);
         cell->list.head = eval_sexpr(env, el);
         cell->list.tail = NIL;
         if (previous)
@@ -732,7 +859,7 @@ struct sexpr* eval_type_argument(struct env* env, struct sexpr* args, int n, enu
     struct sexpr* arg = eval_argument(env, args, n);
     CHECK_ERROR(arg);
     if (arg->tag != type)
-        return new_error("Argument is of wrong type");
+        return new_error(env, "Argument is of wrong type");
     return arg;
 }
 
@@ -745,7 +872,7 @@ struct sexpr* eval_define(struct env* env, struct sexpr* args)
 
     if (sym->tag != symbol)
     {
-        return new_error("Argument not evaluated to symbol");
+        return new_error(env, "Argument not evaluated to symbol");
     }
 
     struct sexpr* value = eval_argument(env, args, 1);
@@ -774,7 +901,7 @@ struct sexpr* call_lambda(struct env* env, struct sexpr* lambda, struct sexpr* a
         if (!arg)
         {
             pop_stack_frame(env);
-            return new_error("Not enough arguments for lambda function");
+            return new_error(env, "Not enough arguments for lambda function");
         }
 
         add_env_binding(env, param->name, arg);
@@ -800,7 +927,7 @@ struct sexpr* eval_loop(struct env* env, struct sexpr* args)
     int arg_count = list_length(args);
 
     if (arg_count < 3)
-        return new_error("loop needs at least 3 arguments");
+        return new_error(env, "loop needs at least 3 arguments");
 
     struct sexpr* params = next(&args);
 
@@ -808,7 +935,7 @@ struct sexpr* eval_loop(struct env* env, struct sexpr* args)
 
     struct sexpr* body = args;
 
-    struct sexpr* l = new_function(lambda); 
+    struct sexpr* l = new_function(env, lambda); 
 
     l->function.lambda.params = params;
     l->function.lambda.exprs = body;
@@ -821,9 +948,22 @@ struct sexpr* eval_recur(struct env* env, struct sexpr* args)
     struct sexpr* lambda = env->stack->context;
 
     if (!lambda)
-        return new_error("recur can only be used inside of lambda");
+        return new_error(env, "recur can only be used inside of lambda");
 
     return call_lambda(env, lambda, args);
+}
+
+struct sexpr* eval_progn(struct env* env, struct sexpr* args)
+{
+    struct sexpr* result = NIL;
+
+    struct sexpr* arg;
+    while ((arg = next(&args)))
+    {
+        result = eval_sexpr(env, arg);
+    }
+
+    return result;
 }
 
 struct sexpr* eval_sexpr(struct env* env, struct sexpr* sexpr)
@@ -846,7 +986,7 @@ struct sexpr* eval_sexpr(struct env* env, struct sexpr* sexpr)
         }
         else
         {
-            return new_error("Non function value found when evaluating list");
+            return new_error(env, "Non function value found when evaluating list");
         }
     }
     else if (sexpr->tag == symbol)
@@ -855,7 +995,7 @@ struct sexpr* eval_sexpr(struct env* env, struct sexpr* sexpr)
         if (!value)
         {
             printf("Unknown symbol: %s\n", sexpr->name);
-            return new_error("Unknown symbol");
+            return new_error(env, "Unknown symbol");
         }
         else
         {
@@ -939,6 +1079,7 @@ struct sexpr* eval_printl(struct env* env, struct sexpr* args)
 
 void set_env(struct env* env)
 {
+    memset(env->heap, 0, sizeof(env->heap));
     env->stack = create_frame();
     add_env_builtin_function(env, "+", eval_add);
     add_env_builtin_function(env, "-", eval_subtract);
@@ -958,6 +1099,7 @@ void set_env(struct env* env)
     add_env_builtin_function(env, "printl", eval_printl);
     add_env_builtin_function(env, "recur", eval_recur);
     add_env_builtin_function(env, "loop", eval_loop);
+    add_env_builtin_function(env, "progn", eval_progn);
 }
 
 void readline(char* buff, size_t size, bool* eof)
@@ -1065,7 +1207,7 @@ int main()
         if (input[0] == '\0')
             continue;
 
-        struct sexpr* e = read_sexpr(&input);
+        struct sexpr* e = read_sexpr(&env, &input);
 
         skip_whitespace(&input);
 
@@ -1083,6 +1225,8 @@ int main()
 
         e = eval_sexpr(&env, e);
         printf("< "); print_sexpr(e); printf("\n");
+
+        collect_garbage(&env);
     }
 
     free_string_builder(&input_builder);
